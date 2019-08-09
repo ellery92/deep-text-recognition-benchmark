a93ca5907cefe0aa54a76d1194aac2a5b3660223
Ellery Wang <elleryq92@gmail.com>
2019-08-09 16:24:06 +0800

    remove debug info

--- test.py
+++ test.py
@@ -87,7 +87,6 @@ def validation(model, criterion, evaluation_loader, converter, opt):
         length_for_pred = torch.IntTensor([opt.batch_max_length] * batch_size).to(device)
         text_for_pred = torch.LongTensor(batch_size, opt.batch_max_length + 1).fill_(0).to(device)
 
-        print ("evaluation:", labels)
         text_for_loss, length_for_loss = converter.encode(labels, batch_max_length=opt.batch_max_length)
 
         start_time = time.time()
